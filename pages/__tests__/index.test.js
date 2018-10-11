d4c59d2cf35cd59faa809a9cdfbfdc8e8d76a9c1
Andrew <oahehc@gmail.com>
2018-10-11 09:33:21 +0800

    react-test-renderer

--- pages/__tests__/index.test.js
+++ pages/__tests__/index.test.js
@@ -1,13 +1,13 @@
 /* eslint-env jest */
 
-import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
 import React from 'react'
 import App from '../index.js'
 
 describe('With Snapshot Testing', () => {
   it('App shows "Hello world!"', () => {
-    const app = shallow(<App />)
-
-    expect(app).toMatchSnapshot()
+    const component = renderer.create(<App />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
